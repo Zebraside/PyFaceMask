a10b1d6655be85e2b440585db93423cfbb1804be
Kirill Molchanov <Kirill_Molchanov@epam.com>
2022-03-22 19:33:55 +0400

    Initial commit

---
+++ Readme.md
@@ -0,0 +1,12 @@
+# PyFaceMask
+
+This project is an attempt to make realtime application to mask face (even on cpu).  
+PyFaceMask is a playground to make poc of the application.
+
+## Used models
+
+### Face detection
+
+### Face segmentation
+
+### 
---
+++ models/image_transform/adapter.py
@@ -0,0 +1,30 @@
+import cv2
+
+import torch
+from torchvision.transforms import transforms
+
+from .image_transform_net import ImageTransformNet
+
+
+class ImageTransform:
+    def __init__(self, weights_path, device='cpu'):
+        self.device = device
+        self.net = ImageTransformNet()
+        self.net.load_state_dict(torch.load(weights_path, map_location=torch.device(device)))
+
+    def transform(self, image):
+        orig_shape = image.shape
+        image = cv2.resize(image, (300, 300))
+        with torch.no_grad():
+            image_transform = transforms.Compose([
+                transforms.ToTensor(),
+                transforms.Lambda(lambda x: x * 255)])
+
+            image_tensor = image_transform(image).unsqueeze(0)
+            if self.device != 'cpu':
+                image_tensor = image_tensor.cuda()
+
+            transformed = self.net(image_tensor).squeeze(0).numpy().clip(0, 255).transpose(1, 2, 0)
+            transformed = cv2.resize(transformed, (orig_shape[1], orig_shape[0]))
+
+        return transformed
